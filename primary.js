baba0c4b471ffa9c0fa227951e5640fd187fd389
Webster Grimes <cwebsterg@mac.com>
2022-09-25 22:31:17 -0400

    I decided I wanted to debug my original code layout so worked a little on that, practiced the new skills.

--- primary.js
+++ primary.js
@@ -1,49 +1,41 @@
 //create empty container for customer directory
 const directory = document.querySelector("#directory");
 
-// create function that will load all customer data into directory
-// function buildDirectory[customerArray] {
-    // for (let customer of customerArray) {
+// go through each customer in the array
         for (let customer of customers) {
-// create the container/div for customer record
         let customerDiv = document.createElement("div");
-
-// add customer to css classes-note these are already defined on the main.css page but this adds them to the html so that the css works!
-        customerDiv.classList.add("customer");
-
-// profile picture: create img element & class, set source
-        let photo = document.createElement("img");
-        customerDiv.classList.add("photo");
-        photo.src = customer.picture.large;
         
-// customer name: create element, add class, define text
-        let fullName = document.createElement("h3");
-        customerDiv.classList.add("h3");
-        fullName.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
-
-// email: create element, add class, define text
+// declare variables and tags for css
+        let photo = document.createElement("img");
+        let fullName = document.createElement("div");
         let email = document.createElement("div");
-        customerDiv.classList.add("email");
-        email.innerText = customer.email;
-
-// address: create elements (2) to handle the street vs city/state but only one class
         let street = document.createElement("div");
         let cityState = document.createElement("div");
-        customerDiv.classList.add("address");
-        street.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
-        cityState.innerText = `${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`;
-
-// dob: create element for dob which will have to be formatted to remove time data
         let dob = document.createElement("div");
-        customerDiv.classList.add("dob");
-        dob.innerText = customer.dob.date;
-
-// dateRegistered: create element for date of registration and format to remove time data
         let dateRegistered = document.createElement("div");
-        customerDiv.classList.add("registered");
-        dateRegistered.innerText = customer.registered.date;
 
-// write the conplete customer record to its own entry box and render all boxes in directory container
+// add necessary css classes (note these are already defined on the main.css page but this adds them to the html so that the css works)
+        customerDiv.classList.add("customer");
+        photo.classList.add("photo");
+        fullName.classList.add("fullName");
+        email.classList.add("email");
+        street.classList.add("address");
+        cityState.classList.add("address");
+        dob.classList.add("dob");
+        dateRegistered.classList.add("registered");
+
+
+// data values mapped to declared variables
+        photo.src = customer.picture.large;
+        fullName.innerText = `${customer.name.first} ${customer.name.last}`;
+        email.innerText = customer.email;
+        street.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
+        cityState.innerText = `${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}`;
+        dob.innerText = `Birthday: ${moment(customer.dob.date).format("MMMM Do YYYY")}`;
+        dateRegistered.innerText = `Customer since ${moment(customer.registered.date).format("MMM Do YYYY")}`;
+
+
+// write the conplete customer record to its own entry box
         customerDiv.appendChild(photo);
         customerDiv.appendChild(fullName);
         customerDiv.appendChild(email);
@@ -51,10 +43,12 @@ const directory = document.querySelector("#directory");
         customerDiv.appendChild(cityState);
         customerDiv.appendChild(dob);
         customerDiv.appendChild(dateRegistered);
+
+// render all customer records in directory container
         directory.appendChild(customerDiv);
 
     }
 
 
-// }
+
 
